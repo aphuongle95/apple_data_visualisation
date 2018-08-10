91bbda05751175cb541821be211bf576d711000d
le anh phuong <annie.le@bizedge.com>
2018-08-10 03:06:03 +0200

    word cloud

--- visual.py
+++ visual.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+import csv
+from collections import OrderedDict
 from operator import itemgetter
 from matplotlib.widgets import Button
-
+import re
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import math
@@ -15,13 +17,37 @@ fig = plt.figure()
 scatterplot = fig.add_subplot(2,2,2)
 wordplot = fig.add_subplot(2,2,4)
 
-with open('apple_store.json') as f:
-    data = json.load(f)
+with open('apple_store.json') as f1:
+    data = json.load(f1)
 
+fieldnames = ("id", "track_name", "size_bytes", "app_desc")
+description_data = []
+with open('description.csv', 'r') as csvfile:
+    #python's standard dict is not guaranteeing any order,
+    #but if you write into an OrderedDict, order of write operations will be kept in output.
+    reader = csv.DictReader(csvfile, fieldnames)
+    for row in reader:
+        entry = {}
+        for field in fieldnames:
+            entry[field] = row[field]
+        description_data.append(entry)
+
+description_data.pop(0)
 countAppDict = {}
 appDict = {}
 
+print(description_data[0])
+print(data[0])
+
+def getDescription(app):
+    for app_w_description in description_data:
+        if str(app_w_description['id']) == str(app['id']):
+            app['description'] = app_w_description['app_desc']
+            break
+    return app
+
 for app in data:
+    app = getDescription(app)
     genre = app['prime_genre']
     if genre in countAppDict:
         count = countAppDict[genre]
@@ -36,7 +62,7 @@ for app in data:
 
 
 countAppDict = sorted(countAppDict.items(), key=itemgetter(1), reverse=True)
-print(countAppDict)
+# print(countAppDict)
 
 num_genre = len(countAppDict)
 grid_col = math.ceil(num_genre/2)
@@ -85,7 +111,7 @@ for app in countAppDict:
             verticalalignment='top',
             transform = ax.transAxes)
 
-# Fixing random state for reproducibility
+#scatter plot
 np.random.seed(19680801)
 
 # N = 50
@@ -98,10 +124,11 @@ x = []
 y = []
 area = []
 genre_data = appDict['Games']
+# print(genre_data[0])
 N = len(genre_data)
 colors = np.random.rand(N)
 
-print(N)
+# print(N)
 for app in genre_data:
     def getRandomXinRange(user_rating):
         u = user_rating*500
@@ -116,5 +143,31 @@ for app in genre_data:
     area.append(app['rating_count_tot']/1000)
 
 scatterplot.scatter(x, y, s=area, c=colors, alpha=0.5)
+
+#word cloud
+import io
+from collections import Counter
+from os import path
+
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+d = path.dirname(__file__)
+
+f = open("description_genre.txt", "w")
+for app in genre_data:
+    if 'description' in app:
+        f.write(app['description'])
+        # print(app['description'])
+
+text = io.open(path.join(d, 'description_genre.txt')).read()
+# Generate a word cloud image
+# The Symbola font includes most emoji
+font_path = path.join(d, 'Symbola.ttf')
+word_cloud = WordCloud(font_path=font_path).generate(text)
+
+# Display the generated image:
+wordplot.imshow(word_cloud)
+wordplot.axis("off")
 
 plt.show()
